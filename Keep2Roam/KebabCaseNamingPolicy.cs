80235ae7f3a9c143935e7f6219459dafe39d5f8a
Michal Bičan <michal@efaut.com>
2020-11-04 13:47:54 +0100

    IK-2: Create RoamResearch models

---
+++ Keep2Roam/KebabCaseNamingPolicy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+
+namespace Keep2Roam
+{
+    // inspiration: https://www.rickvandenbosch.net/blog/creating-a-custom-jsonnamingpolicy/
+    public class KebabCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            var result = new StringBuilder();
+            for (var i = 0; i < name.Length; i++)
+            {
+                var c = name[i];
+                if (i == 0)
+                {
+                    result.Append(char.ToLowerInvariant(c));
+                }
+                else
+                {
+                    if (char.IsUpper(c))
+                    {
+                        result.Append('-');
+                        result.Append(char.ToLowerInvariant(c));
+                    }
+                    else
+                    {
+                        result.Append(c);
+                    }
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
