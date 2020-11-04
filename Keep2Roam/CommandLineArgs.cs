baef64746afb1f4d411914bc02f34bfb51c5e28b
Michal Bičan <michal@efaut.com>
2020-11-04 13:20:02 +0100

    Ik 3 create console app (#3)
    * IK-3: Introduce CommandLineParser
    
    * IK-3: Finish command line app

---
+++ Keep2Roam/CommandLineArgs.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace Keep2Roam
+{
+    public class CommandLineArgs
+    {
+        [Value(0, MetaName = "Files", MetaValue = "FILE FILE2 ...", HelpText = "*.json files from Google Keep Takeout")]
+        public IEnumerable<string> Files { get; set; }
+    }
+}
--- Keep2Roam/Program.cs
+++ Keep2Roam/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using CommandLine;
 using Keep2Roam.Models.GoogleKeep;
 
 namespace Keep2Roam
@@ -9,15 +12,33 @@ namespace Keep2Roam
     {
         public static async Task<int> Main(string[] args)
         {
-            var fileName = args[0];
-            using var file = File.OpenRead(fileName);
-            var model = await JsonSerializer.DeserializeAsync<CardModel>(
-                file,
-                new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                });
+            return await Parser.Default.ParseArguments<CommandLineArgs>(args)
+                .MapResult(
+                    RunAndReturnExitCodeAsync,
+                    _ => Task.FromResult(1));
+        }
+
+        public static async Task<int> RunAndReturnExitCodeAsync(CommandLineArgs args)
+        {
+            var cards = await DeserializeGoogleKeepCardsAsync(args.Files)
+                .ToListAsync()
+                .ConfigureAwait(false);
             return 0;
+        }
+
+        public static async IAsyncEnumerable<CardModel> DeserializeGoogleKeepCardsAsync(IEnumerable<string> fileNames)
+        {
+            foreach (var fileName in fileNames)
+            {
+                using var file = File.OpenRead(fileName);
+                yield return await JsonSerializer.DeserializeAsync<CardModel>(
+                    file,
+                    new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    })
+                    .ConfigureAwait(false);
+            }
         }
     }
 }
